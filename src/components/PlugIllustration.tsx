c9a58ec4e0dc829feae56ba33c8f8a8c848c3782
Jalal Nasser <jnasser1983@hotmail.com>
2025-06-23 01:50:25 +0300

    [dyad] Removed the broken placeholder image from the PlugIllustration component. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/PlugIllustration.tsx
+++ src/components/PlugIllustration.tsx
@@ -4,12 +4,7 @@ const PlugIllustration: React.FC = () => {
   return (
     <div className="relative w-48 h-48 md:w-64 md:h-64 flex items-center justify-center">
       {/* Placeholder for the plug illustration */}
-      <img
-        src="https://via.placeholder.com/200x200?text=Illustration"
-        alt="Abstract illustration"
-        className="w-full h-full object-contain"
-      />
-      {/* Removed the placeholder for stars */}
+      {/* The previous img tag with a placeholder src has been removed. */}
     </div>
   );
 };
