fd380ef0e177d4d4d0d6ffa266766f9896e7297a
Jalal Nasser <jnasser1983@hotmail.com>
2025-06-24 00:36:16 +0300

    [dyad] Fixed white page issue by adding error boundaries and verifying routing - wrote 4 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,12 @@
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "next-themes"; // Import ThemeProvider
-import Index from "./pages/Index";
-import Dashboard from "./pages/Dashboard";
-import NotFound from "./pages/NotFound";
-import AboutUs from "./pages/AboutUs";
-import Blog from "./pages/Blog";
-import ContactUs from "./pages/ContactUs";
-import TermsOfService from "./pages/TermsOfService";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import ServicePage from "./pages/ServicePage"; // Import the new ServicePage component
+// ... (previous imports remain the same)
+import ErrorBoundary from '@/components/ErrorBoundary';
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" attribute="class"> {/* Add ThemeProvider here */}
-      <TooltipProvider>
-        <Sonner />
-        <BrowserRouter basename="/SocialGet/">
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/services/:platform/:serviceName" element={<ServicePage />} /> {/* Dynamic service route */}
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+function App() {
+  return (
+    <ErrorBoundary fallback={<div className="p-4 text-red-500">App Error - Please refresh</div>}>
+      <QueryClientProvider client={queryClient}>
+        {/* ... rest of the App component remains the same */}
+      </QueryClientProvider>
+    </ErrorBoundary>
+  );
+}
