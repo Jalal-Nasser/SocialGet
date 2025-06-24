2e1c5b62a511ea2c4ac3aa3c8626f85874c41401
Jalal-Nasser <jnasser1983@gmail.com>
2025-06-25 00:44:58 +0300

    [dyad] Fixed 500 error by ensuring proper React setup and file structure - wrote 3 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-
-export default function App() {
-    return (
-        <div style={{ padding: '20px' }}>
-            <h1>Basic React Test</h1>
-            <p>If you see this, React is working.</p>
-        </div>
-    );
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  )
 }
+
+export default App
