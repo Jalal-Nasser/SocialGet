e238b8631f4fb6f7ea2358fd6ba4681b91a5bd9a
Jalal-Nasser <jnasser1983@gmail.com>
2025-06-25 00:32:58 +0300

    [dyad] Created static HTML test page to isolate server vs React vs Supabase issues - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import TestConnection from '@/pages/TestConnection';
 
-function App() {
-  return (
-    <div className="min-h-screen bg-white">
-      <TestConnection />
-    </div>
-  );
+export default function App() {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>Basic React Test</h1>
+            <p>If you see this, React is working.</p>
+        </div>
+    );
 }
-
-export default App;
